61367679e29c61063d6dbea2349083f1e2db51c5
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2022-11-16 00:37:03 +0530

    Version update 1.1.5
    Increased gap and block padding for cards.
    Removed unnecessary assets.

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ function App() {
             </main>
             <footer className="footer">
                 <a href="//github.com/shivamdevs" target="_blank" rel="noreferrer">© Shivam Devs 2022</a>
-                <div className="version">Version: 1.1.4</div>
+                <div className="version">Version: 1.1.5</div>
             </footer>
         </div>
     );
